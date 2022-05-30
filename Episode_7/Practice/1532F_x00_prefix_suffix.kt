278488ea2c6b929d932d8e82bd16c01ffdb87030
lordfme <fortmax120@gmail.com>
2022-05-30 15:31:23 +0100

    upload files

--- Episode_7/Practice/1532F_x00_prefix_suffix.kt
+++ Episode_7/Practice/1532F_x00_prefix_suffix.kt
@@ -6,17 +6,17 @@ fun main(){
     val n = readLine()!!.toInt()
     val strings = MutableList(2*n-2){ readLine()!! }
     val stringsSorted = strings.withIndex().sortedBy{ (_, value) -> value.length }
-    var (prefix, suffix) = stringsSorted.takeLast(2)
-    if (prefix.value.substring(1) != suffix.value.substring(0, n-2))
+    var (prefix, suffix) = stringsSorted.map{x -> x.value}.takeLast(2)
+    if (prefix.substring(1) != suffix.substring(0, n-2))
         prefix = suffix.also { suffix = prefix }
     var swap = false
     val ansTmp = MutableList(2*n-2){'X'}
     stringsSorted.chunked(2).forEach{ (subPrefix, subSuffix) ->
-        if (prefix.value.startsWith(subPrefix.value) && suffix.value.endsWith(subSuffix.value)) {
+        if (prefix.startsWith(subPrefix.value) && suffix.endsWith(subSuffix.value)) {
             ansTmp[subPrefix.index] = 'P'; ansTmp[subSuffix.index] = 'S'
         } else {
             ansTmp[subPrefix.index] = 'S'; ansTmp[subSuffix.index] = 'P'
-            if (!(prefix.value.startsWith(subSuffix.value) && suffix.value.endsWith(subPrefix.value)))
+            if (!(prefix.startsWith(subSuffix.value) && suffix.endsWith(subPrefix.value)))
                 swap = true
         }
     }
